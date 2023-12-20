01641ce7af765ccb9acb8a75b550cdce4383a694
nohjuhyeon <njh2720@gmail.com>
2023-12-20 06:43:30 +0000

    과제 제출

--- solving_problems_functions.py
+++ solving_problems_functions.py
@@ -1,5 +1,5 @@
 # 문제 출력/사용자 입력 받는 function
-def problems_m(questions) :      # hint
+def problems_main(questions) :      # hint                              # problems_m -> problemss_main
     
     #list_problems 에서 질문에만 번호 부여하기 위해 인덱스 홀수 짝수로 나눔
     problems_first = questions[1], questions[3], questions[5], questions[7]
@@ -9,7 +9,7 @@ def problems_m(questions) :      # hint
     list_results = []
 
     #각 그룹에서 질문 가져옴
-    for question in range(len(problems_first)):       # hint
+    for question in range(len(problems_first)):       # hint [] -> range(len(problems_first))
         question_a = problems_second[question]
         question_b = problems_first[question]
         
@@ -43,7 +43,7 @@ def problems_m(questions) :      # hint
         # A : 30 이상, B : 20 점 이상,  F : 20점 미만 
         if score >= 30:
             score_result = "A"
-        elif score >= 20:       # hint
+        elif score >= 20:       # hint                                  # else -> elif
             score_result = "B"
         else:
             score_result = "F"
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     # 출제 문제
     list_problems = [
             'Python에서 변수를 선언하는 방법은? (점수: 10점)',
-            '1) var name 2) name = value 3) set name 4) name == value',
+            '1) var name 2) name = value 3) set name 4) name == value',             # '1) var name 2) name = value 3) set name 4) name == value' 리스트 추가
             'Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)',
             '1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다', 
             'Python에서 조건문을 작성하는 방법은? (점수: 10점)', 
@@ -70,8 +70,8 @@ if __name__ == "__main__":
             '1) class, 2) def, 3) import, 4) return'
     ]       # hint
     # 문제 당 정답
-    list_corrects = [4,2,3,2 ]    # hint
+    list_corrects = [4,2,3,2 ]    # hint                                    # list_corrects에 list 추가 
 
 
-    problems_m(list_problems)
+    problems_main(list_problems)
 
